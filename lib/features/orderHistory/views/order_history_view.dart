20de9b880b15044f23104acf424e65756d90d565
Rober Maher <robermaher@gmail.com>
2026-07-15 19:01:07 +0300

    Implement OrderHistoryView with a list of orders and 'Order Again' button

--- lib/features/orderHistory/views/order_history_view.dart
+++ lib/features/orderHistory/views/order_history_view.dart
@@ -1,6 +1,9 @@
 // features/orderHistory/views/order_history_view.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gap/gap.dart';
+import 'package:hungry/shared/custom_button.dart';
+import 'package:hungry/shared/custom_text.dart';
 
 class OrderHistoryView extends StatelessWidget {
   static const String routeName = '/order-history';
@@ -9,9 +12,62 @@ class OrderHistoryView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Order History View', style: TextStyle(fontSize: 24.sp)),
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: ListView.builder(
+            padding: const EdgeInsets.only(bottom: 120, top: 20),
+            itemCount: 5,
+            itemBuilder: (context, index) {
+              return Card(
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 20,
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image.asset(
+                                'assets/test/test1.png',
+                                width: 100.w,
+                              ),
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              CustomText(
+                                text: 'Hamburger',
+                                weight: FontWeight.bold,
+                              ),
+                              CustomText(text: 'Qty: 2'),
+                              CustomText(text: 'Price: \$10.00'),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Gap(20.h),
+                      CustomButton(
+                        onPressed: () {},
+                        text: 'Order Again',
+                        width: double.infinity,
+                        color: Colors.grey.shade400,
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
       ),
     );
   }
