93e44c7e79bc84d7a549e05d12c9d8c12619a3fc
Rober Maher <robermaher@gmail.com>
2026-07-16 19:23:30 +0300

    Refactor SplashView to use StatefulWidget and add animations for logo and image transitions

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,12 +1,67 @@
 // splash.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
 import 'package:hungry/core/constants/app_colors.dart';
+import 'package:hungry/root.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
+  static const String routeName = '/splash';
   const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> with TickerProviderStateMixin {
+  late AnimationController _controller;
+
+  late Animation<double> logoScale;
+  late Animation<double> logoOpacity;
+  late Animation<Offset> imageSlide;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    /// Logo Animation
+    logoScale = Tween<double>(
+      begin: 0.7,
+      end: 1,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOutBack));
+
+    logoOpacity = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
+
+    /// Image Animation
+    imageSlide = Tween<Offset>(
+      begin: const Offset(0, 1),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+
+    _controller.forward();
+
+    /// Navigation
+    Future.delayed(const Duration(seconds: 2), () {
+      if (mounted) {
+        Navigator.pushReplacementNamed(context, Root.routeName);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,12 +72,26 @@ class SplashView extends StatelessWidget {
         child: Column(
           children: [
             Gap(280.h),
-            SvgPicture.asset('assets/icons/logo.svg'),
-            Spacer(),
-            Image.asset(
-              'assets/images/splash_image.png',
+
+            /// Logo Animation
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) {
+                return Opacity(
+                  opacity: logoOpacity.value,
+                  child: Transform.scale(scale: logoScale.value, child: child),
+                );
+              },
+              child: SvgPicture.asset('assets/icons/logo.svg'),
             ),
-            
+
+            const Spacer(),
+
+            /// Image Slide Animation
+            SlideTransition(
+              position: imageSlide,
+              child: Image.asset('assets/images/splash_image.png'),
+            ),
           ],
         ),
       ),
