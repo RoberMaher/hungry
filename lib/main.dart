ae3eb985d929fe7acfd4b83f16eb37b66e7ecedd
Rober Maher <robermaher@gmail.com>
2026-07-19 17:08:24 +0300

    Make main() asynchronous to ensure proper initialization and set preferred device orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 // main.dart
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hungry/my_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
